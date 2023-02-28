f235ecbc33740bef702a5bfa7a86be808e19cb61
Antonio Latela <a.latela@novafutur.com>
2023-02-28 11:48:42 +0100

    minor update

--- test/datastructures/arrays/DynamicIntArrayTest.java
+++ test/datastructures/arrays/DynamicIntArrayTest.java
@@ -4,52 +4,52 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DynamicIntArrayTest {
-
+    DynamicIntArray dynamicIntArray = new DynamicIntArray();
     @Test
     public void createArray() {
         assertArrayEquals("Arrays are not equals",
-                new int[]{1,2,3,4}, new DynamicIntArray().createArray(1,2,3,4));
+                new int[]{1,2,3,4}, dynamicIntArray.createArray(1,2,3,4));
     }
 
     @Test
     public void insertFirst() {
-        assertEquals(3, new DynamicIntArray().insertFirst(3, new int[]{3,2,1})[0]);
+        assertEquals(3, dynamicIntArray.insertFirst(3, new int[]{3,2,1})[0]);
     }
 
     @Test
     public void insertLast() {
-        assertEquals(3, new DynamicIntArray().insertLast(3, new int[]{3,2,1})[3]);
+        assertEquals(3, dynamicIntArray.insertLast(3, new int[]{3,2,1})[3]);
     }
 
     @Test
     public void insertAtIdx() {
-        assertEquals(3, new DynamicIntArray().insertAtIdx(3, 3, new int[]{3,2,1})[3]);
+        assertEquals(3, dynamicIntArray.insertAtIdx(3, 3, new int[]{3,2,1})[3]);
     }
 
     @Test
     public void deleteLast() {
-        assertArrayEquals("Arrays are not equal", new int[]{1,2,3,4}, new DynamicIntArray().deleteLast(1,2,3,4,5));
+        assertArrayEquals("Arrays are not equal", new int[]{1,2,3,4}, dynamicIntArray.deleteLast(1,2,3,4,5));
     }
 
     @Test
     public void deleteFirst() {
-        assertArrayEquals("Arrays are not equal", new int[]{2,3,4}, new DynamicIntArray().deleteFirst(1,2,3,4));
+        assertArrayEquals("Arrays are not equal", new int[]{2,3,4}, dynamicIntArray.deleteFirst(1,2,3,4));
 
     }
 
     @Test
     public void deleteValue() {
-        assertArrayEquals("Arrays are not equal", new int[]{2,3,4}, new DynamicIntArray().deleteValue(1,2,3,4));
+        assertArrayEquals("Arrays are not equal", new int[]{2,3,4}, dynamicIntArray.deleteValue(1,2,3,4));
     }
 
     @Test
     public void insertOrdered() {
-        assertArrayEquals("Arrays are not equal", new int[]{1,2,3,4,5,6}, new DynamicIntArray().insertOrdered(4,3,2,1,5,6));
+        assertArrayEquals("Arrays are not equal", new int[]{1,2,3,4,5,6}, dynamicIntArray.insertOrdered(4,3,2,1,5,6));
     }
 
     @Test
     public void order() {
-        assertArrayEquals("Arrays are not equal", new int[]{1,2,3,4}, new DynamicIntArray().order(4,3,2,1));
+        assertArrayEquals("Arrays are not equal", new int[]{1,2,3,4}, dynamicIntArray.order(4,3,2,1));
     }
 
     @Test
