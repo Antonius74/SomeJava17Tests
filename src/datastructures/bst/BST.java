4aeff101e159bb3a80181a438af81753004b6528
Antonio Latela <a.latela@novafutur.com>
2023-03-03 23:17:32 +0100

    minor update

---
+++ src/datastructures/bst/BST.java
@@ -0,0 +1,67 @@
+package datastructures.bst;
+
+class Node<T> {
+    public Node(T value) {
+        this.value = value;
+    }
+
+    Node<T> left;
+    Node<T> right;
+    T value;
+}
+public class BST<T extends Comparable<T>> {
+    Node<T> head;
+    Node<T> next;
+
+    public int compare(T t1, T t2){
+        return t1.compareTo(t2);
+    }
+
+    void addNode(T val) {
+
+        if (head==null) {
+            head = new Node<>(val);
+        } else {
+            next = head;
+            while (next != null) {
+                if (compare(next.value, val) > 0) {
+                    //System.out.println("val l" + val);
+                    next = next.left;
+                } else if (compare(next.value, val) < 0) {
+                    //System.out.println("val r" + val);
+                    next = next.right;
+                }
+            }
+            next = new Node<>(val);
+            //System.out.println(next.value);
+
+        }
+
+    }
+
+    void readNode() {
+        if (head!=null) {
+            System.out.println(head.value);
+            readNode(head.right);
+            readNode(head.left);
+        }
+    }
+    void readNode(Node<T> node) {
+        //System.out.println(node);
+        if (node != null) {
+            System.out.println(node.value);
+            readNode(node.left);
+            readNode(node.right);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        bst.addNode(1);
+        bst.addNode(2);
+        bst.addNode(3);
+        bst.readNode();
+    }
+
+}
